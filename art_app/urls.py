976ca427b8bfc2c0a2ab28b97be94dd6e372724c
lazerllamaz <reilly.h.gardner@gmail.com>
2023-11-26 13:03:21 -0700

    Updated item view, implemented create item form

--- art_app/urls.py
+++ art_app/urls.py
@@ -4,8 +4,13 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
 
+    #Inventory list and detail views
     path("inventories/", views.InventoryListView.as_view(), name="inventories"),
     path("inventories/<int:inventory_id>/", views.InventoryDetailView, name="inventory-detail"),
 
+    #item detail view
     path("items/<int:pk>/", views.ItemDetailView.as_view(), name="item-detail"),
+
+    #create item form
+    path('inventories/<int:inventory_id>/create_item/', views.createItem, name='create_item'),
 ]
--- art_app/views.py
+++ art_app/views.py
@@ -29,3 +29,27 @@ class ItemListView(generic.ListView):
     model = Item
 class ItemDetailView(generic.DetailView):
     model = Item
+
+
+def createItem(request, inventory_id):
+    form = ItemForm()
+    inventory = Inventory.objects.get(pk=inventory_id)
+
+    if request.method == 'POST':
+        # Create a new dictionary with form data and inventory_id
+        project_data = request.POST.copy()
+        project_data['inventory_id'] = inventory_id
+        
+        form = ItemForm(project_data)
+        if form.is_valid():
+            # Save the form without committing to the database
+            project = form.save(commit=False)
+            # Set the inventory relationship
+            project.inventory = inventory
+            project.save()
+
+            # Redirect back to the portfolio detail page
+            return redirect('inventory-detail', inventory_id)
+
+    context = {'form': form}
+    return render(request, 'art_app/item_form.html', context)
